d89a4657ad707dc665183b1f4ec8bb424dcbda2f
Alice <milesirwin07@berkeley.edu>
2025-10-28 15:35:08 -0700

    added the second ball, some consts and changed background

--- game.py
+++ game.py
@@ -2,14 +2,18 @@ import pygame
 
 pygame.init()
 
-screen_width=500
-screen_height=500
+screen_height = 500
+screen_width = 1000
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("Pool Game")
 #colors
 white = (255, 255, 255)
+black = (0,0,0)
+red = (255, 0, 0)
+green = (0, 255, 0)
 blue = (0, 0, 255)
-
+#ball consts
+ball_rad=15
 #game loop, runs when the game runs and we put stuff in here
 run = True
 while run:
@@ -17,5 +21,11 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    ball1 = pygame.draw.circle(screen,white,(screen_width/2,screen_height/2),15)
+    
+    #setting background
+    screen.fill(white)
+    #creating balls
+    ball1 = pygame.draw.circle(screen,blue,(screen_width/2,screen_height/2),ball_rad)
+    ball2 = pygame.draw.circle(screen,red,(screen_width/4,screen_height/2),ball_rad)
+
     pygame.display.update() #updating the display so we can see changes
