04b3cf7b41cbb9ce9298248a648a911fb86d8f55
Alice <milesirwin07@berkeley.edu>
2025-11-10 19:37:57 -0800

    added wall collision and you can make ball1 move

--- game.py
+++ game.py
@@ -7,20 +7,29 @@ screen_height = 500
 screen_width = 1000
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("Pool Game")
+
 #colors
 white = (255, 255, 255)
 black = (0,0,0)
 red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
-#ball consts
+
+#ball position consts
 ball_rad=15
-ball1_x = random.randint(int(screen_width*3/4+ball_rad),screen_width-ball_rad)
-ball1_y = random.randint(ball_rad,screen_height-ball_rad)
+ball1_x = random.randint(int(screen_width*3/4+ball_rad),screen_width-ball_rad) #start on the right 4th of the screen
+ball1_y = random.randint(ball_rad,screen_height-ball_rad)   #random y pos
 ball1_pos = (ball1_x,ball1_y)
-ball2_x = random.randint(ball_rad,int(screen_width/4-ball_rad))
-ball2_y = random.randint(ball_rad,screen_height-ball_rad)
+ball2_x = random.randint(ball_rad,int(screen_width/4-ball_rad)) #start on the left 4th of the screen
+ball2_y = random.randint(ball_rad,screen_height-ball_rad)   #random y pos
 ball2_pos = (ball2_x,ball2_y)
+#ball velocity consts
+ball1_vx = 0
+ball1_vy = 0
+ball1_vel = (ball1_vx,ball1_vy)
+ball2_vx = 0
+ball2_vy = 0
+ball2_vel = (ball2_vx,ball2_vy)
 #game loop, runs when the game runs and we put stuff in here
 run = True
 while run:
@@ -28,10 +37,33 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    
-    #setting background
+
+    #checking mouse position every frame
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    mouse_pos = (mouse_x, mouse_y)
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.button == 1:
+            ball1_vx = 0.005*(mouse_x-ball1_x)
+            ball1_vy = 0.005*(mouse_y-ball1_y)
+
+    #wall bounces
+    if ball1_x - ball_rad < 0 or ball1_x + ball_rad > screen_width:
+        ball1_vx = -ball1_vx
+    if ball1_y - ball_rad < 0 or ball1_y + ball_rad > screen_height:
+        ball1_vy = -ball1_vy
+    if ball2_x - ball_rad < 0 or ball2_x + ball_rad > screen_width:
+        ball2_vx = -ball2_vx
+    if ball2_y - ball_rad < 0 or ball2_y + ball_rad > screen_height:
+        ball2_vy = -ball2_vy
+    #updating positions of the balls
+    ball1_x += ball1_vx
+    ball1_y += ball1_vy
+    ball1_pos = (ball1_x, ball1_y)
+    ball2_x += ball2_vx
+    ball2_y += ball2_vy
+    ball2_pos = (ball2_x, ball2_y)
+    #drawing stuff
     screen.fill(white)
-    #creating balls
     ball1 = pygame.draw.circle(screen,blue,ball1_pos,ball_rad)
     ball2 = pygame.draw.circle(screen,red,ball2_pos,ball_rad)
 
