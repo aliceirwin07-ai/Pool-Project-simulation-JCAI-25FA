ee627d8a7fb57d47cb05a8f4f71cfd734565889b
Alice <milesirwin07@berkeley.edu>
2025-11-12 21:39:35 -0800

    fixed friction, now the balls come to a rest

--- game.py
+++ game.py
@@ -1,40 +1,43 @@
 import pygame
 import random
 import math
-
+#initialize the screen
 pygame.init()
-
+#set screen variables and stuff
 screen_height = 500
 screen_width = 1000
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("Pool Game")
-
-#colors
+#colors(NEED TO CHANGE BALL COLORS TO BE LESS UGLY)
 white = (255, 255, 255)
 black = (0,0,0)
 red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
-
 #ball position consts
 ball_rad=15
+#ball1
 ball1_x = random.randint(int(screen_width*3/4+ball_rad),screen_width-ball_rad) #start on the right 4th of the screen
 ball1_y = random.randint(ball_rad,screen_height-ball_rad)   #random y pos
 ball1_pos = (ball1_x,ball1_y) #ball pos vector
+#ball2
 ball2_x = random.randint(ball_rad,int(screen_width/4-ball_rad)) #start on the left 4th of the screen
 ball2_y = random.randint(ball_rad,screen_height-ball_rad)   #random y pos
 ball2_pos = (ball2_x,ball2_y) #ball pos vector
 #ball velocity consts
+#ball1
 ball1_vx = 0
 ball1_vy = 0
 ball1_vel = (ball1_vx,ball1_vy)
+#ball2
 ball2_vx = 0
 ball2_vy = 0
 ball2_vel = (ball2_vx,ball2_vy)
-#ball friction acceleration
-ax = 0
-ay = 0
-friction_accel = 0.005
+#ball direction consts: theta reference is right to down dir
+ball1_vtheta = 0   #just setting to 0 as a placeholder  
+ball2_vtheta = 0   #just setting to 0 as a placeholder
+#ball acceleration consts
+friction_accel = 0.001  #acceleration due to friction
 #game loop, runs when the game runs and we put stuff in here
 run = True
 while run:
@@ -43,30 +46,73 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-    #checking mouse position every frame
+    #checking mouse position
     mouse_x, mouse_y = pygame.mouse.get_pos()
     mouse_pos = (mouse_x, mouse_y)
     #allowing you to "hit" the ball
     if event.type == pygame.MOUSEBUTTONDOWN:
         if event.button == 1:   #if left click
-            ball1_vx = 0.005*(mouse_x-ball1_x)
+            ball1_vx = 0.005*(mouse_x-ball1_x) #making the velocities proportional to the dist you click from.
             ball1_vy = 0.005*(mouse_y-ball1_y)
-    #friction on the table(NEED TO REWORK)
-    # if ball1_vx<=2*friction_accel and ball1_vx<=-2*friction_accel:
-    #     ax = 0
-    # if ball1_vx>2*friction_accel:
-    #     ax = -friction_accel
-    # if ball1_vx<-2*friction_accel:
-    #     ax = friction_accel
-    # if ball1_vy<=2*friction_accel and ball1_vy<=-2*friction_accel:
-    #     ay = 0
 
-    # if ball1_vy>2*friction_accel:
-    #     ay = -friction_accel
-    # if ball1_vy<-2*friction_accel:
-    #     ay = friction_accel
-    #PLS FIX
-    #wall bounces
+    #friction on the table(NEED TO REWORK)  
+    ball1_speed = math.sqrt(ball1_vx**2+ball1_vy**2)
+    ball2_speed = math.sqrt(ball2_vx**2+ball2_vy**2)
+    #updating direction velocity of the balls
+    #ball1 theta
+    if ball1_vx !=0:    #theta is undef if vx=0
+        ball1_vtheta = math.atan(ball1_vy/ball1_vx) #angle from horizontal to down
+    if ball1_vx == 0 and ball1_vy > friction_accel: #if v points straight
+        ball1_vtheta = -math.pi/2
+    if ball1_vx == 0 and ball1_vy < friction_accel: #if v points straight
+        ball1_vtheta = math.pi/2
+        #ball2 theta
+    if ball2_vx !=0:    #theta is undef if vx=0
+        ball2_vtheta = math.atan(ball2_vy/ball2_vx) #angle from horizontal to down
+    if ball2_vx == 0 and ball2_vy > friction_accel: #if v points straight
+        ball2_vtheta = -math.pi/2
+    if ball2_vx == 0 and ball2_vy < friction_accel: #if v points straight
+        ball2_vtheta = math.pi/2
+    #checking if we need to apply acceleration
+    #ball1
+    if ball1_speed > friction_accel:#check if speed > accel(if the speed is < accel we treat it as being 0 because it's negligible)
+        if ball1_vx > friction_accel and ball1_vy > friction_accel: #case when vx,ball2_vy are pos
+            ball1_vx-=friction_accel*math.cos(ball1_vtheta)
+            ball1_vy-=friction_accel*math.sin(ball1_vtheta)
+        if ball1_vx < friction_accel and ball1_vy < friction_accel: #case when vx,ball2_vy are neg
+            ball1_vx+=friction_accel*math.cos(ball1_vtheta)
+            ball1_vy+=friction_accel*math.sin(ball1_vtheta)
+        if ball1_vx < friction_accel and ball1_vy > friction_accel: #case when vx pos and vy neg
+            ball1_vx+=friction_accel*math.cos(ball1_vtheta)
+            ball1_vy+=friction_accel*math.sin(ball1_vtheta)
+        if ball1_vx > friction_accel and ball1_vy < friction_accel: #case when vx neg and vy pos
+            ball1_vx-=friction_accel*math.cos(ball1_vtheta)
+            ball1_vy-=friction_accel*math.sin(ball1_vtheta)
+    #ball2
+    if ball2_speed > friction_accel: #check if speed > accel (if speed < accel treat as 0)
+        if ball2_vx > friction_accel and ball2_vy > friction_accel: #vx, vy positive
+            ball2_vx -= friction_accel * math.cos(ball2_vtheta)
+            ball2_vy -= friction_accel * math.sin(ball2_vtheta)
+        if ball2_vx < friction_accel and ball2_vy < friction_accel: #vx, vy negative
+            ball2_vx += friction_accel * math.cos(ball2_vtheta)
+            ball2_vy += friction_accel * math.sin(ball2_vtheta)
+        if ball2_vx < friction_accel and ball2_vy > friction_accel: #vx negative, vy positive
+            ball2_vx += friction_accel * math.cos(ball2_vtheta)
+            ball2_vy += friction_accel * math.sin(ball2_vtheta)
+        if ball2_vx > friction_accel and ball2_vy < friction_accel: #vx positive, vy negative
+            ball2_vx -= friction_accel * math.cos(ball2_vtheta)
+            ball2_vy -= friction_accel * math.sin(ball2_vtheta)
+    #rounding velocity to 0 if the speed is too low
+    #ball1
+    if ball1_speed <= friction_accel:
+        ball1_vx = 0
+        ball1_vy = 0
+    #ball2
+    if ball2_speed <= friction_accel:
+        ball2_vx = 0
+        ball2_vy = 0
+
+    #wall bounces(assuming no energy lost in a bounce)
     if ball1_x - ball_rad < 0 or ball1_x + ball_rad > screen_width:
         ball1_vx = -ball1_vx
     if ball1_y - ball_rad < 0 or ball1_y + ball_rad > screen_height:
@@ -75,6 +121,7 @@ while run:
         ball2_vx = -ball2_vx
     if ball2_y - ball_rad < 0 or ball2_y + ball_rad > screen_height:
         ball2_vy = -ball2_vy
+        
     #ball bounces
     dist_balls = math.sqrt((ball1_x-ball2_x)**2+(ball1_y-ball2_y)**2) 
     normal_x = (ball1_x-ball2_x)/dist_balls
@@ -98,6 +145,7 @@ while run:
         # # ball1_y -= (normal_y * overlap)/2
         # # ball2_x += (normal_x * overlap)/2
         # # ball2_y += (normal_y * overlap)/2
+
     #updating positions of the balls
     ball1_x += ball1_vx
     ball1_y += ball1_vy
@@ -105,7 +153,7 @@ while run:
     ball2_x += ball2_vx
     ball2_y += ball2_vy
     ball2_pos = (ball2_x, ball2_y)
-    #drawing stuff
+    #drawing balls + fill screen
     screen.fill(white)
     ball1 = pygame.draw.circle(screen,blue,ball1_pos,ball_rad)
     ball2 = pygame.draw.circle(screen,red,ball2_pos,ball_rad)
